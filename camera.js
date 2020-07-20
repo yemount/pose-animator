69ec8ea9fb486c7f13f8c4cb55d1ae4ad13cfb23
nguyen-huong <57471582+nguyen-huong@users.noreply.github.com>
2020-07-20 19:28:11 -0400

    Update camera.js

--- camera.js
+++ camera.js
@@ -58,11 +58,11 @@ let nmsRadius = 30.0;
 let mobile = false;
 const stats = new Stats();
 const avatarSvgs = {
-  'girl': girlSVG.default,
-  'boy': boySVG.default,
-  'abstract': abstractSVG.default,
-  'blathers': blathersSVG.default,
-  'tom-nook': tomNookSVG.default,
+    'girl': girlSVG.default,
+    'boy': boySVG.default,
+    'abstract': abstractSVG.default,
+    'blathers': blathersSVG.default,
+    'tom-nook': tomNookSVG.default,
 };
 
 /**
@@ -70,37 +70,37 @@ const avatarSvgs = {
  *
  */
 async function setupCamera() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    throw new Error(
-        'Browser API navigator.mediaDevices.getUserMedia not available');
-  }
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+            'Browser API navigator.mediaDevices.getUserMedia not available');
+    }
 
-  const video = document.getElementById('video');
-  video.width = videoWidth;
-  video.height = videoHeight;
+    const video = document.getElementById('video');
+    video.width = videoWidth;
+    video.height = videoHeight;
 
-  const stream = await navigator.mediaDevices.getUserMedia({
-    'audio': false,
-    'video': {
-      facingMode: 'user',
-      width: videoWidth,
-      height: videoHeight,
-    },
-  });
-  video.srcObject = stream;
+    const stream = await navigator.mediaDevices.getUserMedia({
+        'audio': false,
+        'video': {
+            facingMode: 'user',
+            width: videoWidth,
+            height: videoHeight,
+        },
+    });
+    video.srcObject = stream;
 
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-  });
+    return new Promise((resolve) => {
+        video.onloadedmetadata = () => {
+            resolve(video);
+        };
+    });
 }
 
 async function loadVideo() {
-  const video = await setupCamera();
-  video.play();
+    const video = await setupCamera();
+    video.play();
 
-  return video;
+    return video;
 }
 
 const defaultPoseNetArchitecture = 'MobileNetV1';
@@ -110,11 +110,11 @@ const defaultStride = 16;
 const defaultInputResolution = 200;
 
 const guiState = {
-  avatarSVG: Object.keys(avatarSvgs)[0],
-  debug: {
-    showDetectionDebug: true,
-    showIllustrationDebug: false,
-  },
+    avatarSVG: Object.keys(avatarSvgs)[0],
+    debug: {
+        showDetectionDebug: true,
+        showIllustrationDebug: false,
+    },
 };
 
 /**
@@ -122,28 +122,28 @@ const guiState = {
  */
 function setupGui(cameras) {
 
-  if (cameras.length > 0) {
-    guiState.camera = cameras[0].deviceId;
-  }
+    if (cameras.length > 0) {
+        guiState.camera = cameras[0].deviceId;
+    }
 
-  const gui = new dat.GUI({width: 300});
+    const gui = new dat.GUI({width: 300});
 
-  let multi = gui.addFolder('Image');
-  gui.add(guiState, 'avatarSVG', Object.keys(avatarSvgs)).onChange(() => parseSVG(avatarSvgs[guiState.avatarSVG]));
-  multi.open();
+    let multi = gui.addFolder('Image');
+    gui.add(guiState, 'avatarSVG', Object.keys(avatarSvgs)).onChange(() => parseSVG(avatarSvgs[guiState.avatarSVG]));
+    multi.open();
 
-  let output = gui.addFolder('Debug control');
-  output.add(guiState.debug, 'showDetectionDebug');
-  output.add(guiState.debug, 'showIllustrationDebug');
-  output.open();
+    let output = gui.addFolder('Debug control');
+    output.add(guiState.debug, 'showDetectionDebug');
+    output.add(guiState.debug, 'showIllustrationDebug');
+    output.open();
 }
 
 /**
  * Sets up a frames per second panel on the top-left of the window
  */
 function setupFPS() {
-  stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.getElementById('main').appendChild(stats.dom);
+    stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.getElementById('main').appendChild(stats.dom);
 }
 
 /**
@@ -151,106 +151,85 @@ function setupFPS() {
  * happens. This function loops with a requestAnimationFrame method.
  */
 function detectPoseInRealTime(video) {
-  const canvas = document.getElementById('output');
-  const keypointCanvas = document.getElementById('keypoints');
-  const videoCtx = canvas.getContext('2d');
-  const keypointCtx = keypointCanvas.getContext('2d');
+    const canvas = document.getElementById('output');
+    const keypointCanvas = document.getElementById('keypoints');
+    const videoCtx = canvas.getContext('2d');
+    const keypointCtx = keypointCanvas.getContext('2d');
 
-  canvas.width = videoWidth;
-  canvas.height = videoHeight;
-  keypointCanvas.width = videoWidth;
-  keypointCanvas.height = videoHeight;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+    keypointCanvas.width = videoWidth;
+    keypointCanvas.height = videoHeight;
 
-  async function poseDetectionFrame() {
-    // Begin monitoring code for frames per second
-    stats.begin();
+    async function poseDetectionFrame() {
+        // Begin monitoring code for frames per second
+        stats.begin();
 
-    let poses = [];
-   
-    videoCtx.clearRect(0, 0, videoWidth, videoHeight);
-    // Draw video
-    videoCtx.save();
-    videoCtx.scale(-1, 1);
-    videoCtx.translate(-videoWidth, 0);
-    videoCtx.drawImage(video, 0, 0, videoWidth, videoHeight);
-    videoCtx.restore();
+        let poses = [];
 
-    // Creates a tensor from an image
-    const input = tf.browser.fromPixels(canvas);
-    faceDetection = await facemesh.estimateFaces(input, false, false);
-    let all_poses = await posenet.estimatePoses(video, {
-      flipHorizontal: true,
-      decodingMethod: 'multi-person',
-      maxDetections: 1,
-      scoreThreshold: minPartConfidence,
-      nmsRadius: nmsRadius
-    });
+        // Creates a tensor from an image
+        const input = tf.browser.fromPixels(canvas);
 
-    poses = poses.concat(all_poses);
-    input.dispose();
+        // must be transmitted
+        faceDetection = await facemesh.estimateFaces(input, false, false);
 
-    keypointCtx.clearRect(0, 0, videoWidth, videoHeight);
-    if (guiState.debug.showDetectionDebug) {
-      poses.forEach(({score, keypoints}) => {
-      if (score >= minPoseConfidence) {
-          drawKeypoints(keypoints, minPartConfidence, keypointCtx);
-          drawSkeleton(keypoints, minPartConfidence, keypointCtx);
-        }
-      });
-      faceDetection.forEach(face => {
-        Object.values(facePartName2Index).forEach(index => {
-            let p = face.scaledMesh[index];
-            drawPoint(keypointCtx, p[1], p[0], 2, 'red');
+        let all_poses = await posenet.estimatePoses(video, {
+            flipHorizontal: true,
+            decodingMethod: 'multi-person',
+            maxDetections: 1,
+            scoreThreshold: minPartConfidence,
+            nmsRadius: nmsRadius
         });
-      });
+
+        // must be transmitted
+        poses = poses.concat(all_poses);
+
+        input.dispose();
+
+        keypointCtx.clearRect(0, 0, videoWidth, videoHeight);
+
+        canvasScope.project.clear();
+
+        if (poses.length >= 1 && illustration) {
+            Skeleton.flipPose(poses[0]);
+
+            if (faceDetection && faceDetection.length > 0) {
+                let face = Skeleton.toFaceFrame(faceDetection[0]);
+                illustration.updateSkeleton(poses[0], face);
+            } else {
+                illustration.updateSkeleton(poses[0], null);
+            }
+            illustration.draw(canvasScope, videoWidth, videoHeight);
+        }
+
+        canvasScope.project.activeLayer.scale(
+            canvasWidth / videoWidth,
+            canvasHeight / videoHeight,
+            new canvasScope.Point(0, 0));
+
+        // End monitoring code for frames per second
+        stats.end();
+
+        requestAnimationFrame(poseDetectionFrame);
     }
 
-    canvasScope.project.clear();
-
-    if (poses.length >= 1 && illustration) {
-      Skeleton.flipPose(poses[0]);
-
-      if (faceDetection && faceDetection.length > 0) {
-        let face = Skeleton.toFaceFrame(faceDetection[0]);
-        illustration.updateSkeleton(poses[0], face);
-      } else {
-        illustration.updateSkeleton(poses[0], null);
-      }
-      illustration.draw(canvasScope, videoWidth, videoHeight);
-
-      if (guiState.debug.showIllustrationDebug) {
-        illustration.debugDraw(canvasScope);
-      }
-    }
-
-    canvasScope.project.activeLayer.scale(
-      canvasWidth / videoWidth, 
-      canvasHeight / videoHeight, 
-      new canvasScope.Point(0, 0));
-
-    // End monitoring code for frames per second
-    stats.end();
-
-    requestAnimationFrame(poseDetectionFrame);
-  }
-
-  poseDetectionFrame();
+    poseDetectionFrame();
 }
 
 function setupCanvas() {
-  mobile = isMobile();
-  if (mobile) {
-    canvasWidth = Math.min(window.innerWidth, window.innerHeight);
-    canvasHeight = canvasWidth;
-    videoWidth *= 0.7;
-    videoHeight *= 0.7;
-  }  
+    mobile = isMobile();
+    if (mobile) {
+        canvasWidth = Math.min(window.innerWidth, window.innerHeight);
+        canvasHeight = canvasWidth;
+        videoWidth *= 0.7;
+        videoHeight *= 0.7;
+    }
 
-  canvasScope = paper.default;
-  let canvas = document.querySelector('.illustration-canvas');;
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-  canvasScope.setup(canvas);
+    canvasScope = paper.default;
+    let canvas = document.querySelector('.illustration-canvas');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvasScope.setup(canvas);
 }
 
 /**
@@ -258,51 +237,53 @@ function setupCanvas() {
  * available camera devices, and setting off the detectPoseInRealTime function.
  */
 export async function bindPage() {
-  setupCanvas();
+    setupCanvas();
 
-  toggleLoadingUI(true);
-  setStatusText('Loading PoseNet model...');
-  posenet = await posenet_module.load({
-    architecture: defaultPoseNetArchitecture,
-    outputStride: defaultStride,
-    inputResolution: defaultInputResolution,
-    multiplier: defaultMultiplier,
-    quantBytes: defaultQuantBytes
-  });
-  setStatusText('Loading FaceMesh model...');
-  facemesh = await facemesh_module.load();
+    toggleLoadingUI(true);
+    setStatusText('Loading PoseNet model...');
+    posenet = await posenet_module.load({
+        architecture: defaultPoseNetArchitecture,
+        outputStride: defaultStride,
+        inputResolution: defaultInputResolution,
+        multiplier: defaultMultiplier,
+        quantBytes: defaultQuantBytes
+    });
+    setStatusText('Loading FaceMesh model...');
+    facemesh = await facemesh_module.load();
 
-  setStatusText('Loading Avatar file...');
-  let t0 = new Date();
-  await parseSVG(Object.values(avatarSvgs)[0]);
+    setStatusText('Loading Avatar file...');
+    let t0 = new Date();
+    await parseSVG(Object.values(avatarSvgs)[0]);
 
-  setStatusText('Setting up camera...');
-  try {
-    video = await loadVideo();
-  } catch (e) {
-    let info = document.getElementById('info');
-    info.textContent = 'this device type is not supported yet, ' +
-      'or this browser does not support video capture: ' + e.toString();
-    info.style.display = 'block';
-    throw e;
-  }
+    setStatusText('Setting up camera...');
+    try {
+        video = await loadVideo();
+    } catch (e) {
+        let info = document.getElementById('info');
+        info.textContent = 'this device type is not supported yet, ' +
+            'or this browser does not support video capture: ' + e.toString();
+        info.style.display = 'block';
+        throw e;
+    }
 
-  setupGui([], posenet);
-  setupFPS();
-  
-  toggleLoadingUI(false);
-  detectPoseInRealTime(video, posenet);
+    setupGui([], posenet);
+    setupFPS();
+
+    toggleLoadingUI(false);
+    detectPoseInRealTime(video, posenet);
 }
 
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-FileUtils.setDragDropHandler((result) => {parseSVG(result)});
+FileUtils.setDragDropHandler((result) => {
+    parseSVG(result)
+});
 
 async function parseSVG(target) {
-  let svgScope = await SVGUtils.importSVG(target /* SVG string or file path */);
-  let skeleton = new Skeleton(svgScope);
-  illustration = new PoseIllustration(canvasScope);
-  illustration.bindSkeleton(skeleton, svgScope);
+    let svgScope = await SVGUtils.importSVG(target /* SVG string or file path */);
+    let skeleton = new Skeleton(svgScope);
+    illustration = new PoseIllustration(canvasScope);
+    illustration.bindSkeleton(skeleton, svgScope);
 }
-    
+
 bindPage();
