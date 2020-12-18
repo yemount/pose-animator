03ed7a9d95de35f55efb27110604c9f1fe33411b
Jeremie Papon <jpapon@gmail.com>
2020-12-18 00:17:46 -0800

    Added into list of default avatars

--- camera.js
+++ camera.js
@@ -34,6 +34,7 @@ import * as boySVG from './resources/illustration/boy.svg';
 import * as abstractSVG from './resources/illustration/abstract.svg';
 import * as blathersSVG from './resources/illustration/blathers.svg';
 import * as tomNookSVG from './resources/illustration/tom-nook.svg';
+import * as holidayFrogSVG from './resources/illustration/holiday-frog.svg';
 
 // Camera stream video element
 let video;
@@ -63,6 +64,7 @@ const avatarSvgs = {
   'abstract': abstractSVG.default,
   'blathers': blathersSVG.default,
   'tom-nook': tomNookSVG.default,
+  'holiday-frog':holidayFrogSVG.default,
 };
 
 /**
